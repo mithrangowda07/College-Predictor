163e21cf7f9b672f327de8c09df5883517dcae80
Mithra N Gowda <156055776+mithrangowda07@users.noreply.github.com>
2024-11-26 10:31:34 +0530

    Update college_predictor.py

--- college_predictor.py
+++ college_predictor.py
@@ -6,7 +6,7 @@ import datetime
 st.title("KCET College Cutoff Viewer")
 
 # Define the local file path for the Excel file
-FILE_PATH = "https://raw.githubusercontent.com/mithrangowda07/College-Predictor/main/cet_colg_data.xlsx"
+FILE_PATH = "https://raw.githubusercontent.com/mithrangowda07/College-Predictor/main/cet_colg_data1.xlsx"
 
 # Load the Excel file
 @st.cache_data
